0734f133b512c377a5a39caac4ce3b0e7303dc6d
malinka <malinkaphann@gmail.com>
2021-02-03 13:25:11 +0700

    update car filter

--- topautocam/resources/views/custom-pagination-links.blade.php
+++ topautocam/resources/views/custom-pagination-links.blade.php
@@ -4,18 +4,14 @@
 <nav class="woocommerce-pagination">
     <ul class="page-numbers">
 
-        @if($paginator->onFirstPage())
-        <li>
-            <a class="next page-numbers">←</a>
-        </li>
-        @else
         <li>
             <a class="next page-numbers" wire:click="previousPage">←</a>
         </li>
-        @endif
+
         <li>
             <span aria-current="page" class="page-numbers current">{{ $paginator->currentPage() }}</span>
         </li>
+        
         @if ($paginator->hasMorePages())
         <li>
             <a class="next page-numbers" wire:click="nextPage">→</a>
